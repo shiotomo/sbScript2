0ffcebfa8eb886a4587dbd45e0acf85050ec4995
shiotomo <t.shiota16@gmail.com>
2018-10-02 13:24:07 +0900

    Add .env.sample

--- src/backup.js
+++ src/backup.js
@@ -1,10 +1,13 @@
+require('dotenv').config();
+
 const fs = require('fs-extra');
 
 exports.copyDir = () => {
   // とりあえずhogeディレクトリをpiyoディレクトリにコピーするようにしている
   // 将来的にここを変更
   fs.readFile('./backup-list.txt', 'utf-8', (err, text) => {
+    // fs.copySync('./' + process.env.BACKUP_DIR, './piyo');
     console.log(text);
   });
-  // fs.copySync('./hoge', './piyo');
+  // console.log(process.env.BACKUP_DIR);
 }
